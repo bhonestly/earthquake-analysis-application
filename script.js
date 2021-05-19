86f1cb7e4e43867023591a7639e926831bec8f41
bhonestly <ernestbizzell@gmail.com>
2021-05-19 18:12:42 +0300

    unique locations

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const input = document.querySelector('input')
 const button = document.querySelector('button')
-const locationContainer = document.querySelector('.location-list')
+const locationForm = document.querySelector('#location-form')
+let featuresArray = []
 
 const locationSearch = async () => { 
-  // const url = `${DOMAIN}`
   try {
     const response = await axios.get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson')
     const data = response.data.features
-    // console.log(data)
+    featuresArray = data
     appendData(data)
     return data
   } catch (error) {
@@ -26,21 +26,22 @@ locationSearch()
 // console.log(locations)
 
 
-function appendData(locations) {
-  console.log(locations)
-  let placeArray = []
+function appendData(features) {
+  const uniqueLocations = []            // this empty array will hold unique locations after the features forEach runs
   const dataList = document.querySelector('#select-location')
-  console.log(dataList)
-  locations.forEach(location => {
-    // console.log(location)
-    let locationArray = location.properties.place.split('of')
-    placeArray.push(locationArray[1])
-    console.log(locationArray[1])
-    const optionTag = document.createElement('option')
-    optionTag.setAttribute('value', locationArray[1])
-    dataList.appendChild(optionTag)
-    // let locations = features.properties.place
-    // console.log(location.properties.place)
+  features.forEach(feature => {         // for each item in the API response array we are grabbing the location.
+    let placesArray = feature.properties.place.split('of')
+    if (!uniqueLocations.includes(placesArray[1]))    uniqueLocations.push(placesArray[1])    // for each item in the API response array we are checking if the location is included in the uniqueLocations array.  If it is not in the uniqueLocations array we are pushing the location into that array.
   })
-  // console.log(placeArray)
+  uniqueLocations.forEach(location => {        // for each location in the uniqueLocations array we are creating an option tag, setting the value attribute of the option tag to the location value and appending the option tag to the DOM
+    const optionTag = document.createElement('option')
+    optionTag.setAttribute('value', location)
+    dataList.appendChild(optionTag)
+  })
 }
+
+
+locationForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log(featuresArray)
+})
