b07e4b6a51cd6fa179526cf082abef4f3c4ffcea
bhonestly <ernestbizzell@gmail.com>
2021-05-20 17:44:40 +0300

    type data display

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const input = document.querySelector('input')
 const button = document.querySelector('button')
 const locationForm = document.querySelector('#location-form')
 const magnitudeForm = document.querySelector('#magnitude-form')
+const typeForm = document.querySelector('#type-form')
 let featuresArray = []
 
 const locationSearch = async () => { 
@@ -11,6 +12,7 @@ const locationSearch = async () => {
     featuresArray = data
     appendLocationData(data)
     // appendMagnitudeData(data)
+    appendTypeData(data)
     return data
   } catch (error) {
     console.error(error)
@@ -33,7 +35,6 @@ function appendLocationData(features) {
 }
 
 function displayDataByLocation(filteredFeatures) {
-// display the following data.  Under geometry.coordinates 0:latitude 1:longitude 2:depth, felt, mag, place(beginning of string), tsunami, time
   const dataContainer = document.querySelector('.data-container')
   filteredFeatures.forEach( feature => {
     const time = feature.properties.time
@@ -66,44 +67,23 @@ function displayDataByLocation(filteredFeatures) {
 
 locationForm.addEventListener('submit', (e) => {
   e.preventDefault()
-  console.log(featuresArray)
   const locationInput = document.querySelector('#location-input')
-  console.log(locationInput.value)
   const featuresByLocation = featuresArray.filter( feature => {
     const placesArray = feature.properties.place.split('of')
       return placesArray[1] === locationInput.value
   })
-  console.log(featuresByLocation)
   displayDataByLocation(featuresByLocation)
 })
 
-const magnitudeSearch = async () => { 
-  try {
-    const response = await axios.get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson')
-    const data = response.data.features
-    featuresArray = data
-    appendMagnitudeData(data)
-    return data
-  } catch (error) {
-    console.error(error)
-  }
-}
-// magnitudeSearch()
-
 function appendMagnitudeData() {
-  const uniqueMagnitudes = []            // this empty array will hold unique lmagnitude after the features forEach runs
-  // console.log(featuresArray)
+  const uniqueMagnitudes = []           
   const dataList = document.querySelector('#select-magnitude')
-  featuresArray.forEach(feature => {         // for each item in the API response array we are grabbing the magnitude.
-    // console.log(feature)
-    let magnitude = feature.properties.mag/*.slice('')*/
-    console.log(magnitude)
-    // if (!uniqueMagnitude.includes(magArray[1])) 
-    
-    uniqueMagnitudes.push(magnitude)    // for each item in the API response array we are checking if the magnitude is included in the uniqueMagnitude array.  If it is not in the uniqueMagnitude array we are pushing the location into that array.
+  featuresArray.forEach(feature => {         
+    let magnitude = feature.properties.mag
+    uniqueMagnitudes.push(magnitude)
   })
-  // console.log(uniqueMagnitudes)
-  uniqueMagnitudes.forEach(magnitude => {        // for each magnitude in the uniqueMagnitude array we are creating an option tag, setting the value attribute of the option tag to the location value and appending the option tag to the DOM
+
+  uniqueMagnitudes.forEach(magnitude => {        
     const optionTag = document.createElement('option')
     optionTag.setAttribute('value', magnitude)
     dataList.appendChild(optionTag)
@@ -111,7 +91,6 @@ function appendMagnitudeData() {
 }
 
 function displayDataByMagnitude(filteredFeatures) {
-  // display the following data.  Under geometry.coordinates 0:latitude 1:longitude 2:depth, felt, place(entire string), tsunami, time
     const dataContainer = document.querySelector('.data-container')
     filteredFeatures.forEach( feature => {
       const time = feature.properties.time
@@ -144,15 +123,70 @@ function displayDataByMagnitude(filteredFeatures) {
 
   magnitudeForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    // console.log(featuresArray)
     const maxInput = document.querySelector('#max')
     const minInput = document.querySelector('#min')
-    // console.log(parseInt(maxInput.value))
     const featuresByMaxMagnitude = featuresArray.filter(feature => parseInt(maxInput.value) >= feature.properties.mag)
     const featuresByMinMagnitude = featuresArray.filter(feature => parseInt(minInput.value) <= feature.properties.mag)
-      // console.log(feature.properties.mag)
-    
-    // console.log(featuresByMinMagnitude)
+
     displayDataByMagnitude(featuresByMaxMagnitude)
     displayDataByMagnitude(featuresByMinMagnitude)
+  })
+
+  function appendTypeData(features) {
+    // console.log(features)
+    const uniqueTypes = []            
+    const dataList = document.querySelector('#select-type')
+    features.forEach(feature => {
+      let typeString = feature.properties.type
+      // console.log(typeString)
+      if (!uniqueTypes.includes(typeString))    uniqueTypes.push(typeString)   
+    })
+    uniqueTypes.forEach(type => {        
+      const optionTag = document.createElement('option')
+      optionTag.setAttribute('value', type)
+      dataList.appendChild(optionTag)
+    })
+  }
+
+  function displayDataByType(filteredFeatures) {
+    const dataContainer = document.querySelector('.data-container')
+    filteredFeatures.forEach( feature => {
+      const time = feature.properties.time
+      const distanceFrom = feature.properties.place
+      const latitude = feature.geometry.coordinates[1]
+      const longitude = feature.geometry.coordinates[0]
+      const depth = feature.geometry.coordinates[2]
+      const magnitude = feature.properties.mag
+      const felt = feature.properties.felt
+      const tsunami = feature.properties.tsunami
+      const featureTemplate = `
+      <div class="tsunami">
+        <p>Tsunami Associated: ${tsunami}</p>
+      </div>
+      <div class="time">
+        <p>Time Happened: ${time}</p>
+      </div>
+      <div class="geometry">
+      <p>Distance From: ${distanceFrom}</p>
+      <p>Latitude: ${latitude}</p>
+      <p>Longitude: ${longitude}</p>
+        <p>Depth: ${depth} km</p>
+      </div>
+      <div class="outcome">
+        <p>The Magnitude was: ${magnitude}</p>
+        <p>Was Quake Felt: ${felt}</p>
+      </div>
+      `
+      dataContainer.insertAdjacentHTML('beforeend', featureTemplate)
+    })
+  }
+
+  typeForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const typeInput = document.querySelector('#type-input')
+    const featuresByType = featuresArray.filter( feature => {
+      const typeString = feature.properties.type
+        return typeString === typeInput.value
+    })
+    displayDataByType(featuresByType)
   })
