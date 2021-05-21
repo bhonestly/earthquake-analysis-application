433878fa6b3fc474cdf18c81a859dcb3c5c25b40
bhonestly <ernestbizzell@gmail.com>
2021-05-21 20:11:58 +0300

    remove notes

--- script.js
+++ script.js
@@ -23,16 +23,14 @@ const locationSearch = async () => {
 locationSearch();
 
 function appendLocationData(features) {
-  const uniqueLocations = []; // this empty array will hold unique locations after the features forEach runs
+  const uniqueLocations = [];
   const dataList = document.querySelector("#select-location");
   features.forEach((feature) => {
-    // for each item in the API response array we are grabbing the location.
     let placesArray = feature.properties.place.split("of");
     if (!uniqueLocations.includes(placesArray[1]))
-      uniqueLocations.push(placesArray[1]); // for each item in the API response array we are checking if the location is included in the uniqueLocations array.  If it is not in the uniqueLocations array we are pushing the location into that array.
+      uniqueLocations.push(placesArray[1]); 
   });
   uniqueLocations.forEach((location) => {
-    // for each location in the uniqueLocations array we are creating an option tag, setting the value attribute of the option tag to the location value and appending the option tag to the DOM
     const optionTag = document.createElement("option");
     optionTag.setAttribute("value", location);
     dataList.appendChild(optionTag);
